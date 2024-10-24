3eb9a481cd7b0eadf3ec25f4c9178f3e2871244f
Sax Dhiut <mail@iyxeyl.me>
2024-10-24 20:39:02 +0400

    feat: add new entrypoint component and enhance swipe behavior
    Introduces a new Entrypoint component for initializing the miniApp and viewport. Additionally, modifies StickyApp to disable vertical swipe and improve layout structure.

--- src/components/sticky-app.tsx
+++ src/components/sticky-app.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import { cn } from "@/lib/utils"
-import type { ReactNode } from "react"
+import { type Platform, useLaunchParams, useSwipeBehavior } from "@telegram-apps/sdk-react"
+import { type ReactNode, useEffect, useState } from "react"
 
 export const StickyApp = ({ children }: { children: ReactNode }) => {
+	const launchParams = useLaunchParams(true)
+	const swipeBehavior = useSwipeBehavior(true)
+
+	useEffect(() => {
+		if (swipeBehavior === undefined) return
+
+		swipeBehavior.disableVerticalSwipe()
+	}, [swipeBehavior])
+
 	return (
-		<main className={cn("fixed", "left-0", "top-0", "right-0", "bottom-0", "overflow-x-hidden", "overflow-y-auto")}>
-			<div className={cn("h-[calc(100% + 1px)]")}>
-				<div className={cn("flex", "justify-center", "items-center", "p-4", "pt-1", "h-fit")}>{children}</div>
-			</div>
+		<main className={cn(["fixed", "left-0", "top-0", "right-0", "bottom-0", "overflow-x-hidden", "overflow-y-auto"])}>
+			<div className={cn("container", "max-w-sm", "mx-auto")}>{children}</div>
 		</main>
 	)
 }
