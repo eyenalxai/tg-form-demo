d0757007a07c1704041f8678e534b13a0b9cea9b
Sax Dhiut <mail@iyxeyl.me>
2024-10-24 12:57:38 +0400

    feat: extract demo form component
    Extracted the demo form logic into a separate component for better reusability and code organization. This reduces redundancy and improves maintainability across the codebase.

---
+++ src/components/demo-form.tsx
@@ -0,0 +1,137 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { cn } from "@/lib/utils"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+export const DemoForm = () => {
+	const formSchema = z.object({
+		inputOne: z.string(),
+		inputTwo: z.string(),
+		inputThree: z.string(),
+		inputFour: z.string(),
+		inputFive: z.string(),
+		inputSix: z.string(),
+		inputSeven: z.string()
+	})
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			inputOne: "",
+			inputTwo: "",
+			inputThree: "",
+			inputFour: "",
+			inputFive: "",
+			inputSix: "",
+			inputSeven: ""
+		}
+	})
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className={cn("space-y-6")}>
+				<FormField
+					control={form.control}
+					name="inputOne"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input One</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputTwo"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Two</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputThree"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Three</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputFour"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Four</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputFive"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Five</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputSix"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Six</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="inputSeven"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Input Seven</FormLabel>
+							<FormControl>
+								<Input placeholder="input" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
+	)
+}
