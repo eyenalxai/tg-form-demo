067f56bd69471d20932f660e7c4b4c92c97ecfe3
Sax Dhiut <mail@iyxeyl.me>
2024-10-26 13:27:48 +0400

    refactor: simplify component structure by removing unnecessary logic
    Removed `useLaunchParams` and updated return structure to streamline the `StickyApp` component. This change reduces complexity and enhances maintainability while preserving core functionality.

--- src/components/sticky-app.tsx
+++ src/components/sticky-app.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { type Platform, useLaunchParams, useSwipeBehavior } from "@telegram-apps/sdk-react"
+import { useSwipeBehavior } from "@telegram-apps/sdk-react"
 import { type ReactNode, useEffect, useState } from "react"
 
 export const StickyApp = ({ children }: { children: ReactNode }) => {
-	const launchParams = useLaunchParams(true)
 	const swipeBehavior = useSwipeBehavior(true)
 
 	useEffect(() => {
@@ -14,9 +13,5 @@ export const StickyApp = ({ children }: { children: ReactNode }) => {
 		swipeBehavior.disableVerticalSwipe()
 	}, [swipeBehavior])
 
-	return (
-		<main className={cn(["fixed", "left-0", "top-0", "right-0", "bottom-0", "overflow-x-hidden", "overflow-y-auto"])}>
-			<div className={cn("container", "max-w-sm", "mx-auto")}>{children}</div>
-		</main>
-	)
+	return <div className={cn("container", "max-w-sm", "mx-auto")}>{children}</div>
 }
