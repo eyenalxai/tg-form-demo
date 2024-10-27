cd7eea350293c9fd7f9ffae3b3d349ebda94ed7b
Sax Dhiut <mail@iyxeyl.me>
2024-10-27 14:19:50 +0400

    refactor: simplify structure of animated input component
    Removed unnecessary wrapper div and merged styles to streamline the component. This reduces the component's complexity and improves readability.

--- src/components/motion-form/animated-input.tsx
+++ src/components/motion-form/animated-input.tsx
@@ -52,60 +52,51 @@ export const AnimatedInput = ({
 	}, [focusedField, field.name, setFocus])
 
 	return (
-		<div className={cn("w-full", "relative", "bg-background")}>
-			<motion.div
-				style={{
-					position: focusedField === field.name ? "unset" : "absolute",
-					height: "68px"
-				}}
-				layout
-			/>
-			<motion.div
-				layout={"position"}
-				style={{
-					position: focusedField === field.name ? "fixed" : "unset",
-					top: focusedField === field.name ? "6rem" : "unset",
-					width: focusedField === field.name ? "calc(100% - 2rem)" : "auto",
-					left: focusedField === field.name ? "1rem" : "unset",
-					right: focusedField === field.name ? "1rem" : "unset",
-					zIndex: focusedField === field.name ? 30 : 20,
-					padding: focusedField === field.name ? "0.5rem 1rem 1rem 1rem" : "0",
-					background: "background",
-					borderRadius: focusedField === field.name ? "var(--radius)" : "0"
-				}}
-			>
-				<FormItem className={cn(["transition-all", "duration-500", "ease-in-out"])}>
-					<FormLabel
+		<motion.div
+			layout={"position"}
+			style={{
+				position: focusedField === field.name ? "fixed" : "unset",
+				top: focusedField === field.name ? "6rem" : "unset",
+				width: focusedField === field.name ? "calc(100% - 2rem)" : "auto",
+				left: focusedField === field.name ? "1rem" : "unset",
+				right: focusedField === field.name ? "1rem" : "unset",
+				zIndex: focusedField === field.name ? 30 : 20,
+				padding: focusedField === field.name ? "0.5rem 1rem 1rem 1rem" : "0",
+				background: "background",
+				borderRadius: focusedField === field.name ? "var(--radius)" : "0"
+			}}
+		>
+			<FormItem className={cn(["transition-all", "duration-500", "ease-in-out"])}>
+				<FormLabel
+					className={cn(
+						["transition-all", "duration-500", "ease-in-out"],
+						focusedField !== null && focusedField !== field.name && ["opacity-50", "blur-[2px]"]
+					)}
+				>
+					{field.name}
+				</FormLabel>
+				<FormControl>
+					<Input
+						readOnly={isReadOnly}
+						disabled={focusedField !== null && focusedField !== field.name}
 						className={cn(
-							["transition-all", "duration-500", "ease-in-out"],
-							focusedField !== null && focusedField !== field.name && ["opacity-50", "blur-[2px]"]
+							"duration-500",
+							"ease-in-out",
+							focusedField !== null && focusedField !== field.name && ["pointer-events-none", "blur-[2px]"]
 						)}
-					>
-						{field.name}
-					</FormLabel>
-					<FormControl>
-						<Input
-							readOnly={isReadOnly}
-							disabled={focusedField !== null && focusedField !== field.name}
-							className={cn(
-								"duration-500",
-								"ease-in-out",
-								focusedField !== null && focusedField !== field.name && ["pointer-events-none", "blur-[2px]"]
-							)}
-							onFocus={() => handleFocus(field.name)}
-							onBlur={() => {
-								if (!isReadOnly) {
-									setIsReadOnly(true)
-									handleBlur(onBlur)
-								}
-							}}
-							placeholder={field.name}
-							{...field}
-						/>
-					</FormControl>
-					<FormMessage />
-				</FormItem>
-			</motion.div>
-		</div>
+						onFocus={() => handleFocus(field.name)}
+						onBlur={() => {
+							if (!isReadOnly) {
+								setIsReadOnly(true)
+								handleBlur(onBlur)
+							}
+						}}
+						placeholder={field.name}
+						{...field}
+					/>
+				</FormControl>
+				<FormMessage />
+			</FormItem>
+		</motion.div>
 	)
 }
