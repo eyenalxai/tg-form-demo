94133bfdd16847d43af56ed08dc4f343e968ed23
Sax Dhiut <mail@iyxeyl.me>
2024-10-31 13:25:40 +0400

    feat: improve virtual keyboard visibility handling
    Added debounce to handle keyboard visibility on focus events. Fixed potential memory leak by using useRef to manage timeout in focus event handlers.

--- src/lib/virtual-keyboard.ts
+++ src/lib/virtual-keyboard.ts
@@ -1,13 +1,12 @@
 "use client"
 
+import { useIsIpad } from "@/lib/is-mobile"
 import { type Platform, useLaunchParams } from "@telegram-apps/sdk-react"
-import { useEffect, useState, useSyncExternalStore } from "react"
+import { useEffect, useRef, useState, useSyncExternalStore } from "react"
 
 const isKeyboardInput = (elem: HTMLElement) =>
 	((elem.tagName === "INPUT" || elem.tagName === "TEXTAREA") &&
-		!["button", "submit", "checkbox", "file", "image"].includes(
-			(elem as HTMLInputElement).type
-		)) ||
+		!["button", "submit", "checkbox", "file", "image"].includes((elem as HTMLInputElement).type)) ||
 	elem.hasAttribute("contenteditable")
 
 export const useVirtualKeyboardVisibility = () => {
@@ -19,19 +18,19 @@ export const useVirtualKeyboardVisibility = () => {
 	useEffect(() => {
 		if (launchParams === undefined) return
 
-		const nonMobilePlatforms = [
-			"macos",
-			"tdesktop",
-			"weba",
-			"web",
-			"webk"
-		] as Platform[]
+		const nonMobilePlatforms = ["macos", "tdesktop", "weba", "web", "webk"] as Platform[]
 
 		setIsMobile(!nonMobilePlatforms.includes(launchParams.platform))
 	}, [launchParams])
 
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
 	useEffect(() => {
 		const handleFocusIn = (e: FocusEvent) => {
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current)
+				timeoutRef.current = null
+			}
 			if (e.target) {
 				const target = e.target as HTMLElement
 				if (isKeyboardInput(target)) {
@@ -39,18 +38,23 @@ export const useVirtualKeyboardVisibility = () => {
 				}
 			}
 		}
+
 		const handleFocusOut = (e: FocusEvent) => {
 			if (e.target) {
 				const target = e.target as HTMLElement
 				if (isKeyboardInput(target)) {
-					setIsVisible(false)
+					timeoutRef.current = setTimeout(() => setIsVisible(false), 100)
 				}
 			}
 		}
+
 		document.addEventListener("focusin", handleFocusIn)
 		document.addEventListener("focusout", handleFocusOut)
 
 		return () => {
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current)
+			}
 			document.removeEventListener("focusin", handleFocusIn)
 			document.removeEventListener("focusout", handleFocusOut)
 		}
