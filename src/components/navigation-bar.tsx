a8ad65cb9f831443dee986e7f6125dfd5d1a74ac
Sax Dhiut <mail@iyxeyl.me>
2024-11-04 16:40:32 +0400

    refactor: update isIOS check to use custom hook
    Replaced the isIOS check from launchParams platform with the useIsIOS custom hook for better encapsulation and cleaner code. This change improves maintainability and simplifies the navigation bar logic.

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,19 +1,16 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { useIsIpad } from "@/lib/is-mobile"
+import { useIsIOS, useIsIpad } from "@/lib/is-mobile"
 import { cn } from "@/lib/utils"
 import { useVirtualKeyboardVisibility } from "@/lib/virtual-keyboard"
-import { useLaunchParams } from "@telegram-apps/sdk-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 export const NavigationBar = () => {
 	const isKeyboardVisible = useVirtualKeyboardVisibility()
-	const launchParams = useLaunchParams(true)
-	const isIOS = launchParams?.platform === "ios"
+	const isIOS = useIsIOS()
 	const isIpad = useIsIpad()
-
 	const pathname = usePathname()
 
 	if (isKeyboardVisible && !isIpad) return null
